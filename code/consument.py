a076e03c4f98c7259e48dd177d9a37666dc1d5be
kfijalkowski1 <128588751+kfijalkowski1@users.noreply.github.com>
2026-04-09 15:56:14 +0200

    Update consument.py

--- code/consument.py
+++ code/consument.py
@@ -1,12 +1,14 @@
 import json
 from kafka import KafkaConsumer
-  
+
 if __name__ == '__main__':
-  # Kafka Consumer
-  consumer = KafkaConsumer(
-    'messages',
-    bootstrap_servers='localhost:9092',
-    auto_offset_reset='earliest'
-  )
-  for message in consumer:
-    print(json.loads(message.value))
+    consumer = KafkaConsumer(
+        'messages',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        group_id='my-consumer-group',
+        value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+    )
+
+    for message in consumer:
+        print(message.value)
